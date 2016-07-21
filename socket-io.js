6f1fdd528ce6c1d0d7fbb85fbb0bc8d375639bd2
Ericson Luis Quispe Abad <erluqadev@gmail.com>
2016-07-21 11:12:08 -0500

    chat por grupos

--- socket-io.js
+++ socket-io.js
@@ -37,34 +37,47 @@ module.exports = function(server){
       console.log(socket.id);
       console.log('usuario conectado al chat');
 
+      var emit_msg = function(users, msg, username){
+        console.log('xxxxxxxxx');
+        console.log('emitiendo msg');
+        users.forEach(function(u, i){
+          chat.sockets[u.socket_id].emit('new_msg' , {username : username, msg : msg});
+        });
+      };
+
       socket.on('new_msg', function(data){
-        socket.emit('new_msg', data);
-        socket.broadcast.emit('new_msg', data);
+        console.log(data);
+        for(var i = 0; i < groups.length; i++){
+          if(groups[i].grupo_id == data.grupo_id){
+            var users_connect = groups[i].users_connect;
+            for(var j = 0; j < users_connect.length; j++){
+              if(users_connect[j].socket_id == socket.id){
+                emit_msg(users_connect, data.msg, users_connect[j].username);
+                break;
+              }
+            }
+          }
+        }
       })
 
       socket.on('connect_user_group', function(data){
-        console.log(data);
         var grupo_id = data.grupo_id;
-        console.log(grupo_id);
+        var user = data.user;
+        user.socket_id = socket.id;
         for(var i = 0; i < groups.length; i++){
           if(groups[i].grupo_id == grupo_id){
-            var user = data.user;
-            user.socket_id = socket.id;
-            console.log(socket.id);
             groups[i].users_connect.push(user);
+            console.log(groups[i]);
             var users_connect = groups[i].users_connect;
-            console.log(users_connect);
             for(var j = 0; j < users_connect.length; j++){
-              var socket_id = users_connect['socket_id'];
-              console.log(socket_id);
-              if(users_connect[j].username == user.username && users_connect[j].socket_id != socket.id){
+              var user_socket_id = users_connect[j].socket_id;
+              if(users_connect[j].username == user.username && user_socket_id != socket.id){
                 users_connect.splice(j,1);
                 j--
                 continue;
               }
-              if(chat.sockets[socket_id]){
-                console.log(socket_id);
-                chat.sockets[socket_id].emit('connect_user_group', {usuario : data.user.username});
+              if(chat.sockets[user_socket_id]){
+                chat.sockets[user_socket_id].emit('connect_user_group', {username : user.username, grupo_id : grupo_id});
               }
             }
             break;
--- sources/javascripts/chat.js
+++ sources/javascripts/chat.js
@@ -10,36 +10,54 @@ var socket = io.connect("http://192.168.1.58:3000/chat", {
 
 socket.on('new_msg', function(data){
   console.log(data);
-  var span_new_msg =  $('<div>').html(data.msg);
-  $(".content_chat").append(span_new_msg);
+  var div_new_msg =  $('<div>');
+  var span_user = $('<span class="username">').text(data.username);
+  var span_msg = $('<span class="msg">').text(data.msg);
+  div_new_msg.append(span_user);
+  div_new_msg.append(span_msg);
+  $(".content_chat").append(div_new_msg);
 });
 
 socket.on('connect_user_group', function(data){
-  var user = sessionStorage.getItem("user");
-  if(data.usuario == user.username){
+  console.log('connect_user_group');
+  console.log(data);
+  var username = sessionStorage.getItem("username");
+  if(data.username == username){
+    sessionStorage.setItem('grupo_id' , data.grupo_id);
     $(".content_grupos").hide('slow');
-    $(".content_chat").show('slow');
+    $(".content_chat").show('slow').find('h3').html("Chat del Grupo " + data.grupo_id).data('id', data.grupo_id);
     $(".content_input_chat").show('slow');
+  }
+  else{
+    $("#alert_user_connect").html("<strong>" + data.username + "</strong>" + " acaba de conectarse")
+                            .show('slow');
+    setTimeout(function(){
+      $("#alert_user_connect").hide('slow');
+    }, 1000);
   }
 });
 
 
 $(function(){
   $('#btn_send_msg').on('click', function(){
-    var msg = $(".content_input_msg input").val();
-    console.log(msg);
+    var msg = $(".content_input_chat input").val();
     if(msg == ''){
       alert('ingrese mensaje');
       return;
     }
-    socket.emit('new_msg', {msg : msg});
-    $(".content_input_msg input").val('')
+    var grupo_id = sessionStorage.getItem('grupo_id');
+    socket.emit('new_msg', {msg : msg, grupo_id : grupo_id});
+    $(".content_input_chat input").val('')
   });
-  $(".content_input_msg input").on('keyup', function(e){
+
+
+  $(".content_input_chat input").on('keyup', function(e){
     if(e.keyCode == 13){
       $('#btn_send_msg').trigger('click');
     }
   })
+
+
   $("#btn_send_login").on('click', function(){
     var username = $('input[name=usuario]').val();
     var clave = $('input[name=clave]').val();
@@ -53,9 +71,9 @@ $(function(){
       data : {username : username, clave: clave},
       dataType : 'json',
       success : function(respJson){
-        console.log(respJson);
         if(respJson.msg == 'success'){
-          sessionStorage.setItem('user', respJson.user);
+          sessionStorage.setItem('username', respJson.user.username);
+          sessionStorage.setItem('clave', respJson.user.clave);
           respJson.user.groups.forEach(function(g, i){
             var div = $('<div>').html('Grupo ' + g);
             div.data('id' , g);
@@ -63,11 +81,13 @@ $(function(){
           });
           $(".content_login").hide('slow');
           $(".content_grupos").show('slow');
+
+
           $(".content_grupos div").on('click', function(){
             var id = $(this).data('id');
-            console.log('click ' + id);
-            var user = sessionStorage.getItem('user');
-            socket.emit('connect_user_group', {'user' : user, 'grupo_id' : id});
+            var username = sessionStorage.getItem('username');
+            var clave = sessionStorage.getItem('clave');
+            socket.emit('connect_user_group', {'user' : {username : username, clave : clave}, 'grupo_id' : id});
           });
         }
       },
